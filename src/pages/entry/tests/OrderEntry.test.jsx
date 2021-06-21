99a779326786fa3d1980e839a2804f5fa7de370f
Glen Adamson <glenadamson1984@gmail.com>
2021-06-21 21:20:18 +0100

    order entry tests and waitfor

---
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
+
+test("handles errors for scoops and toppings routes", async () => {
+  server.resetHandlers(
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  render(<OrderEntry />);
+
+  //get by - cause its there on initial render
+  //query - not to be there right away
+  // find cause its asynchronous
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(2);
+  });
+});
